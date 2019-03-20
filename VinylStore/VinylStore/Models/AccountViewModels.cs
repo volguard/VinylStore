f94818ea0476413c98f89cc4599419ec1938a8ea
Paweł Kurzyński <pkurzynzjan@example.com>
2019-03-20 19:35:19 +0100

    Models Revival

--- VinylStore/VinylStore/Models/AccountViewModels.cs
+++ VinylStore/VinylStore/Models/AccountViewModels.cs
@@ -49,9 +49,9 @@ namespace VinylStore.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,9 +65,17 @@ namespace VinylStore.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User roles")]
+        public string UserRoles { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
